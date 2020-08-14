08fa02ebdc5e48dec1d0be6d68546df993b3874e
unknown <6110110463@psu.ac.th>
2020-08-14 22:48:41 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import WordCard from './WordCard';
+
+const word  = "Hello";
 function App() {
   return (
-    <div >
-    Hello
+    <div>
+      <WordCard value={word}/>
     </div>
   );
 }
